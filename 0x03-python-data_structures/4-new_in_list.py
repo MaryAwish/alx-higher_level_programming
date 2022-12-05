82739f34364b5eed3064e89bca252b4e84c52421
Mary_Ouma <maryawish54@gmail.com>
2022-12-05 04:09:28 -0800

    replace element and retain copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    i = len(my_list)
+    copy_lst = my_list.copy()
+
+    if idx < 0:
+        return copy_lst
+    elif idx >= i - 1:
+        return copy_lst
+    else:
+        copy_lst[idx] = element
+        return copy_lst
