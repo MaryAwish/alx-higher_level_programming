f1f59dc29245e48be24b3210d36733144be64f0d
Mary_Ouma <maryawish54@gmail.com>
2022-12-05 06:30:26 -0800

    Python objects

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,6 +1,13 @@
 #include <Python.h>
 #include <object.h>
 #include <listobject.h>
+/**
+ * print_python_list_info - prints info about python lists
+ *
+ * @p: pyobject pointer
+ *
+ * Return: void
+ */
 
 void print_python_list_info(PyObject *p)
 {
