da4eed793284718b8f7a884cf866e69f9db9314c
Mary Ouma <maryawish54@gmail.com>
2023-01-10 07:40:55 -0800

    Input and output

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -36,12 +36,12 @@ try:
 
             if str(status) in status_codes.keys():
                 status_codes[str(status)] += 1
-        except:
+        except IndexError:
             pass
 
         try:
             file_size += int(pieces[-1])
-        except:
+        except (IndexError, ValueError):
             pass
 
         lc += 1
